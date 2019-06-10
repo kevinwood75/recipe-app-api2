3780c3989be911bd112a542f1dad0b5621e2b5c4
Kevin Wood <kevin.wood75@gmail.com>
2019-06-10 01:20:40 -0400

    testing

--- app/app/settings.py
+++ app/app/settings.py
@@ -121,7 +121,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
